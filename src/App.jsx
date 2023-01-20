4574c96257234676e610a775fb9263eb36930b3f
Arik Alexandrov <arikxl@gmail.com>
2023-01-20 17:28:48 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-import { logo} from './assets'
+
+import { HomePage, CreatePostPage } from './pages'
+import { Header } from './components';
+
 const App = () => {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <>
+      <BrowserRouter>
+      <Header />
+        <main className="sm:-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)">
+          <Routes>
+            <Route path="/" element={<HomePage/> }/>
+            <Route path="/create-post" element={<CreatePostPage/> }/>
+          </Routes> 
+      </main>
+      </BrowserRouter>
+    </>
   )
 }
 
